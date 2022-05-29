83111f3358bca8619e7b1764ad71adcc3004eb01
smeea-2018 <smeeaarshad@gmail.com>
2022-05-29 20:23:57 +0100

    steps to deliver functionality

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+const renderCities = () => {
+  // Get recent cities [] form LS
+  //If empty render alert
+  // else render recent cities
+  //   Add event listener to div containing all cities
+};
+
+const renderWeatherData = (cityName) => {
+  //Use API to fetch current weather data---
+  //from the response put all data in an object
+  //   get lat and long from weather data API response ....2nd API call
+  //   const weatherForecastUrl =
+  //render data
+};
+
+const renderCurrentWeather = (currentWeatherData) => {
+  // render current weather data and append to the section
+};
+
+const renderForecastWeather = (forecastWeatherData) => {
+  // render forecast weather data and append  each card to the section
+};
+const handleFormSubmit = () => {
+  // get city name from input
+  // if empty
+  // render weather data
+};
+const onReady = () => {
+  // render recent cities
+};
+// on load
+$(document).ready(onReady);
