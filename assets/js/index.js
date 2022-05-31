19b0ce002928fa78548e6f12311f4cb2ce003025
smeea-2018 <smeeaarshad@gmail.com>
2022-05-31 17:32:49 +0100

    current section is dynamcially rendered

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,34 @@
 // target html elements
 const recentSearchContainer = $("#recent-search");
-const searchForm = $("city-search-form");
+const searchForm = $("#city-search-form");
+const weatherInfoContainer = $("#weather-info-container");
+const removeAlert = $("#alert-ul");
+
+// get data from LS
+const getFromLocalStorage = (key, defaultValue) => {
+  const dataFromLS = localStorage.getItem(key);
+
+  const parsedData = JSON.stringify(dataFromLS);
+
+  if (parsedData) {
+    return parsedData;
+  } else {
+    return defaultValue;
+  }
+};
+
+// write to LS
+const writeToLocalStorage = (key, value) => {
+  const stringifiedValue = JSON.stringify(value);
+  localStorage.setItem(stringifiedValue);
+};
 const renderCities = () => {
   // Get recent cities [] form LS
-  const recentSearches = getFromLocalStorage("recentSearches", []);
+  //const recentSearches = getFromLocalStorage("recentSearches", []);
+
+  const recentSearches = ["london", "leeds"];
+  console.log(recentSearches);
+  console.log(recentSearches.length);
   //If past search exists render cities
   if (recentSearches.length) {
     const createRecentCity = (city) => {
@@ -12,6 +37,7 @@ const renderCities = () => {
         </li>`;
     };
     const recentCities = recentSearches.map(createRecentCity).join("");
+    console.log(recentCities);
     const ul = `<ul class="list-group">
             ${recentCities}
           </ul>`;
@@ -19,7 +45,7 @@ const renderCities = () => {
   }
   // else render alert
   else {
-    const alert = `<div class="alert alert-info" role="alert">
+    const alert = `<div class="alert alert-info" role="alert" id = "alert-ul">
           Data has not been found. Please enter the city.
         </div>`;
     recentSearchContainer.append(alert);
@@ -33,6 +59,39 @@ const renderWeatherData = (cityName) => {
   //   const weatherForecastUrl =
   //render data
 };
+const renderCurrentData = () => {
+  console.log("hello");
+  const currentWeatherCard = `<div class="current-weather">
+            <h2>
+              Birmingham 20/05/2022 weather icon
+              <img
+                class="bg-light border rounded"
+                src="http://openweathermap.org/img/w/04d.png"
+                alt="weather icon"
+              />
+            </h2>
+            <div class="row g-0">
+              <div class="col-sm-12 col-md-4 p-2">Temp:</div>
+              <div class="col-sm-12 col-md-8 p-2">16&deg;c</div>
+            </div>
+            <div class="row g-0">
+              <div class="col-sm-12 col-md-4 p-2">Wind:</div>
+              <div class="col-sm-12 col-md-8 p-2">10 MPH</div>
+            </div>
+            <div class="row g-0">
+              <div class="col-sm-12 col-md-4 p-2">Humidity:</div>
+              <div class="col-sm-12 col-md-8 p-2">0&percnt;</div>
+            </div>
+            <div class="row g-0">
+              <div class="col-sm-12 col-md-4 p-2">UV Index:</div>
+
+              <div class="col-sm-12 col-md-8 p-2">
+                <span class="bg-success text-white rounded-2"> 1.5</span>
+              </div>
+            </div>
+          </div>`;
+  weatherInfoContainer.append(currentWeatherCard);
+};
 
 const renderCurrentWeather = (currentWeatherData) => {
   // render current weather data and append to the section
@@ -43,41 +102,25 @@ const renderForecastWeather = (forecastWeatherData) => {
 };
 const handleFormSubmit = (event) => {
   event.preventDefault();
+  console.log("hello");
   // get city name from input
-  const cityName = $("#city-name").value();
+  const cityName = $("#city-name").val();
+  console.log(cityName);
   // if  not empty
   if (cityName) {
+    renderCurrentData();
     const recentSearches = getFromLocalStorage("recentSearches", []);
     recentSearches.push(cityName);
     writeToLocalStorage("recentSearches", recentSearches);
     // remove last section
-    recentSearchContainer.children().last().remove();
+    removeAlert.remove();
+    //recentSearchContainer.children().last().remove();
     // rerender recent cities
     renderCities();
   }
   // render weather data
 };
 
-const getFromLocalStorage = (key, defaultValue) => {
-  const dataFromLS = localStorage.getItem(key);
-
-  const parsedData = JSON.stringify(dataFromLS);
-
-  if (parsedData) {
-    return parsedData;
-  } else {
-    return defaultValue;
-  }
-};
-
-const writeToLocalStorage = (key, value) => {
-  const stringifiedValue = JSON.stringify(value);
-  localStorage.setItem(stringifiedValue);
-};
-const onReady = () => {
-  // render recent cities
-  renderCities();
-};
 const handleSearchClick = (event) => {
   const target = $(event.target);
 
@@ -86,6 +129,10 @@ const handleSearchClick = (event) => {
   }
 };
 
+const onReady = () => {
+  // render recent cities
+  renderCities();
+};
 recentSearchContainer.click(handleSearchClick);
 
 searchForm.submit(handleFormSubmit);
